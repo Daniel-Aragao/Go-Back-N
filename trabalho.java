87315bac6bf2af6371ce6e29883a054db7300e0b
daniel-aragao <danielfilhoce@gmail.com>
2017-11-12 15:53:11 -0300

    Reader e Writter

---
+++ trabalho.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class trabalho {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException{
+		String fileName = args[0];
+		System.out.println("Lendo: " + fileName);
+
+		ArrayList<String> linhas = Leitor.getLines(fileName);
+		
+		GoBackN gbn = new GoBackN(linhas);
+
+		gbn.start();
+
+		Escritor.writeLines(gbn.getSaida());
+	}
+
+}
+
+class Leitor{
+	public static ArrayList<String> getLines(String path) throws IOException{
+		ArrayList <String> retorno = new ArrayList<String>();
+
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		try{
+			String line = br.readLine();
+		
+			while (line != null) {
+				retorno.add(line);
+				line = br.readLine();
+			}
+		}catch(FileNotFoundException e){
+			System.out.println("File Not Found");
+		}finally{
+			br.close();
+		}
+		return retorno;
+	}
+}
+
+class Escritor{
+	public static void writeLines(ArrayList<String> saida) throws IOException{
+		PrintWriter pw = new PrintWriter(new FileWriter("saida"));
+		for (String linha : saida) {
+			pw.write(linha);
+			pw.println();
+		}
+		pw.close();
+	}
+}
+
+class GoBackN{
+	ArrayList<String> Linhas;
+	ArrayList<String> Saida;
+
+	public GoBackN(ArrayList<String> linhas){
+		Linhas = linhas;
+	}
+
+	public ArrayList<String> getSaida(){
+		return Linhas;
+	}
+
+	public void start(){
+
+	}
+}
